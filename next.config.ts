9dfb85d33c352dfb458c03b4dad2c90624fe548c
Sharad Rajput <sharadrajput7042568316@gmail.com>
2026-05-10 12:19:45 +0530

    simplify next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  outputFileTracingExcludes: {
-    "*": ["./public/**/*"],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
