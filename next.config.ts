6001db24d9436c6ac881715cbca168c40a304868
Sharad Rajput <sharadrajput7042568316@gmail.com>
2026-05-10 12:11:25 +0530

    fix build config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    outputFileTracingExcludes: {
-      "*": ["./public/**/*"],
-    },
+  outputFileTracingExcludes: {
+    "*": ["./public/**/*"],
   },
 };
 
